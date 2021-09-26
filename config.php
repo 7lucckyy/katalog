f794e067023f25f619061abfc601a6b4e0c22268
7lucckyy <7lucckyy6611@gmail.com>
2021-09-25 18:04:02 -0700

    live DB config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "katalog";
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "baaedcd2546d24";
+$password = "3d922c37";
+$dbname = "heroku_35826186c5287ef";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
