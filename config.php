a7a5f48fa911405d6957cf88bbb55e6e51750789
7luckyy <7luckyy6611@gmail.com>
2021-10-02 16:22:24 +0100

    db reconfig

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "us-cdbr-east-04.cleardb.com";
-$username = "baaedcd2546d24";
-$password = "3d922c37";
-$dbname = "heroku_35826186c5287ef";
+$username = "bafc164b0508da";
+$password = "6804adaf";
+$dbname = "heroku_7f95f92435e21e3";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
