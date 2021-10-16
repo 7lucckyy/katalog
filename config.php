3f3c98b530c7db4d3e01b54654e0fae65ffce657
7luckyy <7luckyy6611@gmail.com>
2021-10-16 16:59:09 +0100

    Conneted to DB live

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "us-cdbr-east-04.cleardb.com";
-$username = "bafc164b0508da";
-$password = "6804adaf";
-$dbname = "heroku_7f95f92435e21e3";
+$username = "bffc20313bc21a";
+$password = "a8ffd044";
+$dbname = "heroku_a17091ef0145058";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
