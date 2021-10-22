65fc5f0f00b0b4677e71076144b9e3fbfdc15af5
7luckyy <7luckyy6611@gmail.com>
2021-10-22 12:53:50 +0100

    commit local

--- config.php
+++ config.php
@@ -1,4 +1,20 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "katalog";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+else{
+    // echo ("SUCCESS");
+}
+?>
+
+
+<!-- 
+<?php
 $servername = "us-cdbr-east-04.cleardb.com";
 $username = "bffc20313bc21a";
 $password = "a8ffd044";
@@ -10,4 +26,4 @@ if (!$conn) {
 else{
     // echo ("SUCCESS");
 }
-?>
+?> -->
