5d0c3ae2816f6e2b9a19e2a50c57a71c1199243e
7luckyy <7luckyy6611@gmail.com>
2021-10-22 14:09:06 +0100

    config

--- config.php
+++ config.php
@@ -11,19 +11,3 @@ else{
     // echo ("SUCCESS");
 }
 ?>
-
-
-<!-- 
-<?php
-$servername = "us-cdbr-east-04.cleardb.com";
-$username = "bffc20313bc21a";
-$password = "a8ffd044";
-$dbname = "heroku_a17091ef0145058";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-else{
-    // echo ("SUCCESS");
-}
-?> -->
